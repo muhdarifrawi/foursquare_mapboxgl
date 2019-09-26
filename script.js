d9fd0d5d2f2b7931502555065b350f818d61c087
Ubuntu <ubuntu@ip-172-31-37-216.ec2.internal>
2019-09-26 09:35:00 +0000

    added testAPI(). API is working.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 /*global mapboxgl*/
+/*global axios*/
 
 mapboxgl.accessToken="pk.eyJ1IjoibXVoZGFyaWZyYXdpIiwiYSI6ImNrMHl3dDNycTBpdDkzaHJ4cHJtdnU0YXoifQ.8ceqA9s8SqMSbVa1rsZgvg"
 
@@ -12,3 +13,11 @@ let map = new mapboxgl.Map({
 let m = new mapboxgl.Marker()
    .setLngLat([103.8198, 1.3521])
    .addTo(map);
+   
+function testAPI(){
+    axios.get("https://api.foursquare.com/v2/venues/explore?client_id=KU0V5J1DMMBMFD2XR2YNPDDQKIQJE5CMIKNB32YITZTXYXL4&client_secret=4CDZXMM212D1JTYD0VJ055IA00G1EPIYLZRP2Q5ZR1FT3JCS&v=20180323&limit=1&ll=40.7243,-74.0018&query=coffee")
+.then(function(response){
+    console.log(response.data);
+});
+
+}
