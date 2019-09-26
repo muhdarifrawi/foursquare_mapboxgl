3a37955374f54efa6ba13a7b14fa54a789170d6b
Ubuntu <ubuntu@ip-172-31-37-216.ec2.internal>
2019-09-26 04:49:05 +0000

    Mapbox loaded. Map is showing.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("hi");
+/*global mapboxgl*/
+
+mapboxgl.accessToken="pk.eyJ1IjoibXVoZGFyaWZyYXdpIiwiYSI6ImNrMHl3dDNycTBpdDkzaHJ4cHJtdnU0YXoifQ.8ceqA9s8SqMSbVa1rsZgvg"
+
+let map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v9",
+    center: [103.8198, 1.3521],
+    zoom: 10.5
+});
