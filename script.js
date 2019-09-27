e182c827bc49a5c58b9b8ff9849b88086ce398e5
Ubuntu <ubuntu@ip-172-31-37-216.ec2.internal>
2019-09-27 08:37:32 +0000

    added testFourSqAPI. Tested through console. Working as intended.

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 /*global mapboxgl*/
 /*global axios*/
 
+/*Foursquare API. Constants are kept Capsed to differentiate them from Variables.*/
+const API_URL_FSQ = "https://api.foursquare.com/v2";
+const CLIENT_ID = "KU0V5J1DMMBMFD2XR2YNPDDQKIQJE5CMIKNB32YITZTXYXL4";
+const CLIENT_SECRET = "4CDZXMM212D1JTYD0VJ055IA00G1EPIYLZRP2Q5ZR1FT3JCS";
+
 mapboxgl.accessToken="pk.eyJ1IjoibXVoZGFyaWZyYXdpIiwiYSI6ImNrMHl3dDNycTBpdDkzaHJ4cHJtdnU0YXoifQ.8ceqA9s8SqMSbVa1rsZgvg"
 
 let map = new mapboxgl.Map({
@@ -14,10 +19,20 @@ let m = new mapboxgl.Marker()
    .setLngLat([103.8198, 1.3521])
    .addTo(map);
    
-function testAPI(){
-    axios.get("https://api.foursquare.com/v2/venues/explore?client_id=KU0V5J1DMMBMFD2XR2YNPDDQKIQJE5CMIKNB32YITZTXYXL4&client_secret=4CDZXMM212D1JTYD0VJ055IA00G1EPIYLZRP2Q5ZR1FT3JCS&v=20180323&limit=1&ll=40.7243,-74.0018&query=coffee")
+function testFourSqAPI(){
+    axios.get(API_URL_FSQ + "/venues/explore", {
+        params: {
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET, 
+            "v":'20180323' , 
+            "limit": 10 ,
+            "ll": '1.3521, 103.8198' ,
+            "query": 'coffee'
+        }
+    })
 .then(function(response){
-    console.log(response.data);
+    console.log(response.data.response.groups[0].items);
 });
 
 }
+
